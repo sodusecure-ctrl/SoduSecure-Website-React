dd7e5846a7b50951d0387503b13e39ba2b84a6a8
SoduSecure <sodusecure@gmail.com>
2026-03-07 00:27:10 +0100

    fix: use dangerouslySetInnerHTML for GTM script to ensure direct head injection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -129,13 +129,15 @@ export default async function RootLayout({
     <html lang={locale}>
       <head>
         {/* Google Tag Manager */}
-        <Script id="gtm" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id=GTM-K5F9VXCT'+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K5F9VXCT');`}
-        </Script>
+})(window,document,'script','dataLayer','GTM-K5F9VXCT');`,
+          }}
+        />
         {/* End Google Tag Manager */}
 
         {/* Favicon - Alle Browser */}
